a7f15f32dff0b752240a3f9332e3ad85f19e8ad3
Adrian Eguez <eadepto@hotmail.com>
2017-05-02 16:55:31 -0500

    Conexiones

--- Aplicacion1/config/connections.js
+++ Aplicacion1/config/connections.js
@@ -56,14 +56,11 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  // someMongodbServer: {
-  //   adapter: 'sails-mongo',
-  //   host: 'localhost',
-  //   port: 27017,
-  //   user: 'username', //optional
-  //   password: 'password', //optional
-  //   database: 'your_mongo_db_name_here' //optional
-  // },
+  conexionMongoDbDocker: {
+     adapter: 'sails-mongo',
+     host: 'localhost',
+     port: 27017
+   },
 
   /***************************************************************************
   *                                                                          *
@@ -74,12 +71,12 @@ module.exports.connections = {
   *                                                                          *
   *                                                                          *
   ***************************************************************************/
-   somePostgresqlServer: {
+  conexionPostgresqlDocker: {
      adapter: 'sails-postgresql',
      host: 'localhost',
-     user: 'YOUR_POSTGRES_USER', // optional
-     password: 'YOUR_POSTGRES_PASSWORD', // optional
-     database: 'YOUR_POSTGRES_DB' //optional
+     user: 'adrian', // optional
+     password: '123456', // optional
+     database: 'politecnica' //optional
    }
 
 
