ba95e5e18cde440e0035e67b4f3b3691fc762234
USRDEL <USRDEL@USRDEL-Profesor.local>
2017-05-17 14:40:07 -0500

    HTML y Components

--- 04-Angular/A-Web/src/app/app.component.ts
+++ 04-Angular/A-Web/src/app/app.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'twj-raiz',
-  template: `
-  <h1>Hola</h1>
-    
-  `,
+  templateUrl: "./app.component.html" ,
   styleUrls: ['./app.component.css']
 })
 
