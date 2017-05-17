3a5c42a3b1bcad6444bb30e8cb48afdad77a9e54
USRDEL <USRDEL@USRDEL-Profesor.local>
2017-05-17 14:16:16 -0500

    Cambio de selector en el componente

--- 04-Angular/A-Web/src/app/app.component.ts
+++ 04-Angular/A-Web/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'twj-raiz',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
