9913890900c92a8195345e3f8b3f206c0f8aae1d
USRDEL <USRDEL@USRDEL-Profesor.local>
2017-05-17 14:32:22 -0500

    Componentes ANgular2

--- 04-Angular/A-Web/src/app/app.component.ts
+++ 04-Angular/A-Web/src/app/app.component.ts
@@ -2,14 +2,42 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'twj-raiz',
-  templateUrl: './app.component.html',
+  template: `
+  <h1>Hola</h1>
+    
+  `,
   styleUrls: ['./app.component.css']
 })
 
-
-
 export class AppComponent {
-  title = 'Hola amigos';
-  nombre:string = "Adrian";
-  apellido:string = "Eguez";
+  usuario:UsuarioInt = {
+    nombre:"Adrian",
+    apellido:"Eguez"
+  }
+
+  constructor(){
+    console.log("Constructor");
+    this.holaMundo();
+    this.saludar(this.usuario.nombre,
+      this.usuario.apellido
+    );
+  }
+
+  holaMundo(){
+    console.log("Hola mundo");
+  }
+  saludar(nombre:string,
+          apellido?:string):string{
+    return `Hola ${nombre} ${apellido}`;
+  }
+
 }
+
+
+
+
+interface UsuarioInt{
+  nombre:string,
+  apellido:string
+}
+
