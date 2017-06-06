1f788dab15e73aaff111e2a4723b691b9b9c0677
USRDEL <USRDEL@USRDEL-Profesor.local>
2017-06-06 16:24:08 -0500

    Componente Star Wars

---
+++ 04-Angular/A-Web/src/app/Componentes/planeta-star-wars/planeta-star-wars.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-planeta-star-wars',
+  templateUrl: './planeta-star-wars.component.html',
+  styleUrls: ['./planeta-star-wars.component.css']
+})
+export class PlanetaStarWarsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- 04-Angular/A-Web/src/app/app.module.ts
+++ 04-Angular/A-Web/src/app/app.module.ts
@@ -6,13 +6,15 @@ import {HttpModule} from "@angular/http";
 import {NgModule} from "@angular/core";
 import { EstiloComponent } from './estilo/estilo.component';
 import {InicioComponent} from "./Componentes/inicio/inicio.component";
+import { PlanetaStarWarsComponent } from './Componentes/planeta-star-wars/planeta-star-wars.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     EstiloComponent,
-    InicioComponent
+    InicioComponent,
+    PlanetaStarWarsComponent
   ],
   imports: [
     BrowserModule,
