b0f379e8cb7fab670803fdc3f38334bfdcf05bad
Darwin <darwin.morocho@epn.edu.ec>
2017-06-20 16:05:00 -0500

    Actualizar usuarios

--- 04-Angular/A-Web/src/app/Classes/UsuarioClass.ts
+++ 04-Angular/A-Web/src/app/Classes/UsuarioClass.ts
@@ -8,7 +8,8 @@ export class UsuarioClass {
   constructor(public nombre?:string,
               public id?:number,
               public createdAt?:Date,
-              public updatedAt?:Date){
+              public updatedAt?:Date,
+              public editar?:boolean){
   }
 
 }
--- 04-Angular/A-Web/src/app/Componentes/inicio/inicio.component.ts
+++ 04-Angular/A-Web/src/app/Componentes/inicio/inicio.component.ts
@@ -1,6 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {Http} from "@angular/http";
+
+
+
 import 'rxjs/add/operator/map';
+
+
+
+
 import {PlanetaStarWarsInterface} from "../../Interfaces/PlanetaStarWars";
 import {UsuarioClass} from "../../Classes/UsuarioClass";
 
@@ -58,7 +65,24 @@ export class InicioComponent implements OnInit {
         respuesta=>{
           let rjson:UsuarioClass[] = respuesta.json();
 
-          this.usuarios = rjson;
+          this.usuarios = rjson.map(
+            (usuario:UsuarioClass)=>{
+              //cambiar el usuario
+              usuario.editar = false;
+              return usuario;
+            }
+          );
+
+          /*
+          //anadir propiedades a objetos
+
+           let objeto1:any = {
+           prop1:1,
+           prop2:2
+           }
+
+           objeto1.prop3 = 3;
+           */
 
           console.log("Usuarios: ",this.usuarios);
         },
@@ -150,6 +174,7 @@ export class InicioComponent implements OnInit {
         respuesta=>{
           let respuestaJson = respuesta.json()
           this.usuarios.push(respuestaJson);
+          this.nuevoUsuario = new UsuarioClass();
           console.log('respuestaJson: ',respuestaJson);
         },
         error=>{
@@ -171,16 +196,37 @@ export class InicioComponent implements OnInit {
         }
       )
 
+  }
+
+  actualizarUsuario(usuario:UsuarioClass){
+
+    let actualizacion = {
+      nombre:usuario.nombre
+    };
+
+    this._http.put(
+      "http://localhost:1337/Usuario/"+usuario.id,actualizacion)
+      .map(
+        (res)=>{
+          return res.json();
+        })
+      // snippet -> template de codigo para reutilizarlo
+      .subscribe(
+        res=>{
+          //el servidor nos dice que se actualizo
+          console.log("El usuario se actualizo",res);
+        },
+        err=>{
+          //hubo algun problema (Red servidor)
+          console.log("Hubo un error",err);
+        }
 
 
-
-
+      );
 
   }
 
 }
-
-
 
 
 
