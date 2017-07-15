69a90a769168e7d3d00770a9c4ec9ff0b5dd3fd4
Darwin <darwin.morocho@epn.edu.ec>
2017-07-15 10:38:51 -0500

    Componentes login y ruta login

--- 04-Angular/A-Web/src/app/app.module.ts
+++ 04-Angular/A-Web/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { Pagina21Component } from './Componentes/pagina21/pagina21.component';
 import { Pagina22Component } from './Componentes/pagina22/pagina22.component';
 import { Pagina23Component } from './Componentes/pagina23/pagina23.component';
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import { LoginComponent } from './Componentes/login/login.component';
 
 
 @NgModule({
@@ -29,7 +30,8 @@ import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
     Pagina2Component,
     Pagina21Component,
     Pagina22Component,
-    Pagina23Component
+    Pagina23Component,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
